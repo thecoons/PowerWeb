6bc474479e67b9a1c4f22730fb734f065f1c5b66
thecoons <antobarth@gmail.com>
2016-07-04 02:07:16 +0200

    Fix de la gestion des fichiers statics

--- clientWeb/urls.py
+++ clientWeb/urls.py
@@ -2,11 +2,13 @@
 
 from django.conf.urls import url
 from . import views
+from django.views.generic import RedirectView
 
 app_name = "clientWeb"
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
+url(r'^$', views.index, name='index'),
+url(r'^accounts/profile/$', RedirectView.as_view(url='/')),
+url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
 ]
